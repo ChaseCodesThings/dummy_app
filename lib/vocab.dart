9955b89e2bc006f8f2ae24845faab00d394c2d5a
ChaseCodesThings <114005017+ChaseCodesThings@users.noreply.github.com>
2022-12-07 22:24:07 -0500

    dummy app vocab file success

--- lib/vocab.dart
+++ lib/vocab.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:translator/translator.dart';
-import 'mytranslator.dart';
+
 class Vocab extends StatefulWidget {
   @override
   _VocabState createState() => _VocabState();
@@ -10,11 +10,13 @@ class _VocabState extends State<Vocab>{
   int position = 0;
   List<String> picture = ['assets/dog.png', 'assets/cat.png', 'assets/water.png', 'assets/fire.png'];
   GoogleTranslator translator = GoogleTranslator();
+  String translation = '';
+  String out = '';
   @override
   Widget build(BuildContext context) {
     String current = vocab[position];
-    //var translation = '';
-    String out = '';
+
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Vocabulary"),
@@ -44,15 +46,12 @@ class _VocabState extends State<Vocab>{
                   ),
                 ),
                 onPressed: () {
-                    //translation = current;
-                    translator.translate(current, to: "es").then((translation) {
-                      //out = translation.toString();
-                      setState(() {
-                        out = translation.toString();
-                      });
-                      print(out);
+                  translator.translate(current, to: "es").then((translation) {
+                    setState(() {
+                      out = translation.toString();
                     });
-
+                    print(out);
+                  });
                 }
               ),
               Text(out,
@@ -71,6 +70,7 @@ class _VocabState extends State<Vocab>{
                     position++;
                     position %= (vocab.length);
                     current = vocab[position];
+                    out = '';
                   });
                 },
               ),
@@ -85,6 +85,7 @@ class _VocabState extends State<Vocab>{
                     position--;
                     position %= (vocab.length);
                     current = vocab[position];
+                    out = '';
                   });
                 },
                 //child: Image.asset('assets/dog5.jpg')
