c9798ea1adc8f91fd3691e9c90d8a905e673c8d1
ChaseCodesThings <114005017+ChaseCodesThings@users.noreply.github.com>
2022-12-13 11:15:45 -0500

    dummy app game progress

--- lib/game.dart
+++ lib/game.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'ball.dart';
 import 'brick.dart';
 import 'coverscreen.dart';
@@ -12,6 +13,9 @@ class Game extends StatefulWidget {
 enum direction{UP, DOWN}
 
 class _GameState extends State<Game>{
+  //player variables
+  double playerX = 0;
+
   // ball variables
   double ballX = 0.0;
   double ballY = 0.0;
@@ -21,9 +25,6 @@ class _GameState extends State<Game>{
   void startGame() {
     gameStarted = true;
     Timer.periodic(Duration(milliseconds: 1), (timer) {
-      setState(() {
-        ballY += 0.01;
-      });
       //update direction
       updateDirection();
       //move ball
@@ -31,41 +32,69 @@ class _GameState extends State<Game>{
     });
   }
   void updateDirection() {
-    if (ballY >= 0.9) {
-      ballDirection = direction.DOWN;
-    }
-    else if (ballY <= -0.9) {
-      ballDirection = direction.UP;
-    }
+    setState(() {
+      if (ballY >= 0.9) {
+        ballDirection = direction.UP;
+      }
+      else if (ballY <= -0.9) {
+        ballDirection = direction.DOWN;
+      }
+    });
   }
   void moveBall() {
+    //vertical movement
     setState(() {
       if (ballDirection == direction.DOWN) {
         ballY += 0.01;
       }
       else if (ballDirection == direction.UP) {
-        ballX -= 0.01;
+        ballY -= 0.01;
       }
     });
   }
+
+  void moveLeft() {
+    setState(() {
+      playerX -= 0.1;
+    });
+  }
+
+  void moveRight() {
+    setState(() {
+      playerX += 0.1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: startGame,
-      child: Scaffold(
-        backgroundColor: Colors.deepPurpleAccent,
-        body: Center(
-          child: Stack(
-            children: [
-              //tap to play
-              CoverScreen(gameStarted: gameStarted,),
-              //top brick
-              Brick(x: 0.0, y: -0.9),
-              //bottom brick
-              Brick(x: 0.0, y: 0.9),
-              //ball
-              Ball(x: ballX, y: ballY),
-            ],
+    return RawKeyboardListener(
+      focusNode: FocusNode(),
+      autofocus: true,
+      onKey: (event) {
+        if (event.isKeyPressed(LogicalKeyboardKey.arrowLeft)) {
+          moveLeft();
+        }
+        else if (event.isKeyPressed(LogicalKeyboardKey.arrowRight)) {
+          moveRight();
+        }
+      },
+      child: GestureDetector(
+        onTap: startGame,
+        child: Scaffold(
+          backgroundColor: Colors.deepPurpleAccent,
+          body: Center(
+            child: Stack(
+              children: [
+                //tap to play
+                CoverScreen(gameStarted: gameStarted,),
+                //top brick
+                Brick(x: 0.0, y: -0.9),
+                //bottom brick
+                Brick(x: playerX, y: 0.9),
+                //ball
+                Ball(x: ballX, y: ballY),
+              ],
+            ),
           ),
         ),
       ),
